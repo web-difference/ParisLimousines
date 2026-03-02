ebc6c02a19d5167ba189a8121eda8a292607d125
Tristan Wehrle <tastroplaragebaaah@macbook-pro-de-tristan-2.home>
2026-03-02 11:13:46 +0100

    [FIX] sitemap at www

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://star-limousine-paris.com',
+  siteUrl: 'https://www.star-limousine-paris.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 }
